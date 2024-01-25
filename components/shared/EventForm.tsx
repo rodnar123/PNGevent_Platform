b1ac563cfd447318bb8bfb039c05809880a5e6ab
rodnar123 <rodney.naro@gmail.com>
2024-01-25 17:13:21 +1000

    updated EventFormProps-eventId?

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -35,7 +35,7 @@ type EventFormProps = {
   userId: string;
   type: "Create" | "Update";
   event?: IEvent;
-  eventId: string;
+  eventId?: string;
 };
 
 const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
